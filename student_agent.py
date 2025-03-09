a81857a72cb1a605a3082144f219ad4167298b07
gcobs104628 <gcobs104628@gmail.com>
2025-03-09 13:46:32 +0800

    test torch

--- student_agent.py
+++ student_agent.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 import random
 import gym
+import torch
 
 def get_action(obs):
     
@@ -11,8 +12,7 @@ def get_action(obs):
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
-
-
+    
     return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 
