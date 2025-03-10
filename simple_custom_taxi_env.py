bfd83af70dbb752be5acb9b866cb544431fd27af
gcobs104628 <gcobs104628@gmail.com>
2025-03-10 19:42:29 +0800

    udate code

--- simple_custom_taxi_env.py
+++ simple_custom_taxi_env.py
@@ -3,7 +3,7 @@ import numpy as np
 import importlib.util
 import time
 from IPython.display import clear_output
-
+import random
 # This environment allows you to verify whether your program runs correctly during testing, 
 # as it follows the same observation format from `env.reset()` and `env.step()`. 
 # However, keep in mind that this is just a simplified environment. 
@@ -12,136 +12,146 @@ from IPython.display import clear_output
 # You are free to modify this file to better match the real environment and train your own agent. 
 # Good luck!
 
-import gym
-import numpy as np
-import importlib.util
-import time
-from IPython.display import clear_output
 
-class SimpleTaxiEnv(gym.Wrapper):
-    def __init__(self, fuel_limit=5000):
-        self.grid_size = 5
-        env = gym.make("Taxi-v3", render_mode="ansi") #🚨 Taxi-v3 is **always 5x5**. If you want a different grid size, you must create a custom environment.
-        super().__init__(env)
-        
+class SimpleTaxiEnv():
+    def __init__(self, grid_size=5, fuel_limit=50):
+        """
+        Custom Taxi environment supporting different grid sizes.
+        """
+        self.grid_size = grid_size
         self.fuel_limit = fuel_limit
         self.current_fuel = fuel_limit
-
+        self.passenger_picked_up = False
+        
         self.stations = [(0, 0), (0, self.grid_size - 1), (self.grid_size - 1, 0), (self.grid_size - 1, self.grid_size - 1)]
         self.passenger_loc = None
-        self.passenger_picked_up = False  
+       
         self.obstacles = set()  # No obstacles in simple version
         self.destination = None
-    def reset(self, **kwargs):
-        obs, info = super().reset(**kwargs)
+
+    def reset(self):
+        """Reset the environment, ensuring Taxi, passenger, and destination are not overlapping obstacles"""
         self.current_fuel = self.fuel_limit
-
-        taxi_row, taxi_col, pass_idx, dest_idx = self.env.unwrapped.decode(obs)
-
-        taxi_row = min(taxi_row, self.grid_size - 1)
-        taxi_col = min(taxi_col, self.grid_size - 1)
-
-        
-        self.passenger_loc = self.stations[pass_idx] 
-        self.passenger_picked_up = False  
-        self.destination = self.stations[dest_idx]
-        destination_x, destination_y = self.destination
+        self.passenger_picked_up = False
         
 
-        obstacle_north = int(taxi_row == 0)
-        obstacle_south = int(taxi_row == self.grid_size - 1)
-        obstacle_east  = int(taxi_col == self.grid_size - 1)
-        obstacle_west  = int(taxi_col == 0)
+        available_positions = [
+            (x, y) for x in range(self.grid_size) for y in range(self.grid_size)
+            if (x, y) not in self.stations and (x, y) not in self.obstacles
+        ]
 
-        return (taxi_row, taxi_col, *self.passenger_loc, destination_x, destination_y,
-                obstacle_north, obstacle_south, obstacle_east, obstacle_west), info
+        self.taxi_pos = random.choice(available_positions)
+        
+        self.passenger_loc = random.choice([pos for pos in self.stations])
+        
+        
+        possible_destinations = [s for s in self.stations if s != self.passenger_loc]
+        self.destination = random.choice(possible_destinations)
+        
+        return self.get_state(), {}
 
-    def get_state(self):
-        taxi_row, taxi_col, _, _ = self.env.unwrapped.decode(self.env.unwrapped.s)
-        passenger_x, passenger_y = self.passenger_loc
-        destination_x, destination_y = self.destination
-        obstacle_north = int(taxi_row == 0)
-        obstacle_south = int(taxi_row == self.grid_size - 1)
-        obstacle_east  = int(taxi_col == self.grid_size - 1)
-        obstacle_west  = int(taxi_col == 0)
-        return (taxi_row, taxi_col, passenger_x, passenger_y, destination_x, destination_y,
-                obstacle_north, obstacle_south, obstacle_east, obstacle_west)
     def step(self, action):
         """Perform an action and update the environment state."""
-        taxi_row, taxi_col, pass_idx, dest_idx = self.env.unwrapped.decode(self.env.unwrapped.s)
-
+        taxi_row, taxi_col = self.taxi_pos
         next_row, next_col = taxi_row, taxi_col
-        if action == 0 :  # Move South
+        reward = 0
+        if action == 0 :  # Move Down
             next_row += 1
-        elif action == 1:  # Move North
+        elif action == 1:  # Move Up
             next_row -= 1
-        elif action == 2 :  # Move East
+        elif action == 2:  # Move Right
             next_col += 1
-        elif action == 3 :  # Move West
+        elif action == 3:  # Move Left
             next_col -= 1
-
-        if action in [0, 1, 2, 3]:  
-            if not (0 <= next_row < self.grid_size and 0 <= next_col < self.grid_size):
-                reward = -5  
-                self.current_fuel -= 1
-                if self.current_fuel <= 0:
-                    return self.get_state(), reward -10, True, False, {}  
-                return self.get_state(), reward, False, False, {}
-
-        taxi_row, taxi_col = next_row, next_col
-
-        self.current_fuel -= 1  
-        obs, reward, terminated, truncated, info = super().step(action)
-
-        if reward == 20:  
-            reward = 50
-        elif reward == -1:  
-            reward = -0.1
-        elif reward == -10:  
-            reward = -10
-
-        if action == 4:  
-            if pass_idx == 4:  
-                self.passenger_picked_up = True  
-                self.passenger_loc = (taxi_row, taxi_col)  
+        
+        
+        if action in [0, 1, 2, 3]:  # Only movement actions should be checked
+            if (next_row, next_col) in self.obstacles or not (0 <= next_row < self.grid_size and 0 <= next_col < self.grid_size):
+                reward -=5
             else:
-                self.passenger_picked_up = False  
+                self.taxi_pos = (next_row, next_col)
+                if self.passenger_picked_up:
+                    self.passenger_loc = self.taxi_pos
+        else:
+            if action == 4:  # PICKUP
+                if self.taxi_pos == self.passenger_loc:
+                    self.passenger_picked_up = True
+                    self.passenger_loc = self.taxi_pos  
+                else:
+                    reward = -10  
+            elif action == 5:  # DROPOFF
+                if self.passenger_picked_up:
+                    if self.taxi_pos == self.destination:
+                        reward += 50
+                        return self.get_state(), reward -0.1, True, {}
+                    else:
+                        reward -=10
+                    self.passenger_picked_up = False
+                    self.passenger_loc = self.taxi_pos
+                else:
+                    reward -=10
+                    
+        reward -= 0.1  
 
-        elif action == 5:  
-            if self.passenger_picked_up:  
-                self.passenger_picked_up = False  
-                self.passenger_loc = (taxi_row, taxi_col)
+        self.current_fuel -= 1
+        if self.current_fuel <= 0:
+            return self.get_state(), reward -10, True, {}
 
-        if self.passenger_picked_up:
-            self.passenger_loc = (taxi_row, taxi_col)  
+        
 
-        destination_x, destination_y = self.destination
+        return self.get_state(), reward, False, {}
 
-        obstacle_north = int(taxi_row == 0)
-        obstacle_south = int(taxi_row == self.grid_size - 1)
-        obstacle_east  = int(taxi_col == self.grid_size - 1)
-        obstacle_west  = int(taxi_col == 0)
+    def get_state(self):
+        """Return the current environment state."""
+        taxi_row, taxi_col = self.taxi_pos
+        passenger_row, passenger_col = self.passenger_loc
+        destination_row, destination_col = self.destination
+        
+        obstacle_north = int(taxi_row == 0 or (taxi_row-1, taxi_col) in self.obstacles)
+        obstacle_south = int(taxi_row == self.grid_size - 1 or (taxi_row+1, taxi_col) in self.obstacles)
+        obstacle_east  = int(taxi_col == self.grid_size - 1 or (taxi_row, taxi_col+1) in self.obstacles)
+        obstacle_west  = int(taxi_col == 0 or (taxi_row , taxi_col-1) in self.obstacles)
 
-        state = (taxi_row, taxi_col, self.passenger_loc[0], self.passenger_loc[1], destination_x, destination_y,
-                 obstacle_north, obstacle_south, obstacle_east, obstacle_west)
+        passenger_loc_north = int((taxi_row - 1, taxi_col) == self.passenger_loc)
+        passenger_loc_south = int((taxi_row + 1, taxi_col) == self.passenger_loc)
+        passenger_loc_east  = int((taxi_row, taxi_col + 1) == self.passenger_loc)
+        passenger_loc_west  = int((taxi_row, taxi_col - 1) == self.passenger_loc)
+        passenger_loc_middle  = int( (taxi_row, taxi_col) == self.passenger_loc)
+        passenger_look = passenger_loc_north or passenger_loc_south or passenger_loc_east or passenger_loc_west or passenger_loc_middle
+       
+        destination_loc_north = int( (taxi_row - 1, taxi_col) == self.destination)
+        destination_loc_south = int( (taxi_row + 1, taxi_col) == self.destination)
+        destination_loc_east  = int( (taxi_row, taxi_col + 1) == self.destination)
+        destination_loc_west  = int( (taxi_row, taxi_col - 1) == self.destination)
+        destination_loc_middle  = int( (taxi_row, taxi_col) == self.destination)
+        destination_look = destination_loc_north or destination_loc_south or destination_loc_east or destination_loc_west or destination_loc_middle
 
-        return state, reward, terminated, truncated, info
-
-    def render_env(self, taxi_pos, passenger_pos, destination_pos, action=None, step=None, fuel=None):
+        
+        state = (taxi_row, taxi_col, self.stations[0][0],self.stations[0][1] ,self.stations[1][0],self.stations[1][1],self.stations[2][0],self.stations[2][1],self.stations[3][0],self.stations[3][1],obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look)
+        return state
+    def render_env(self, taxi_pos,   action=None, step=None, fuel=None):
         clear_output(wait=True)
 
         grid = [['.'] * self.grid_size for _ in range(self.grid_size)]
-
+        
+        '''
         # Place passenger
         py, px = passenger_pos
         if 0 <= px < self.grid_size and 0 <= py < self.grid_size:
             grid[py][px] = 'P'
-
+        '''
+        
+        
+        grid[0][0]='R'
+        grid[0][4]='G'
+        grid[4][0]='Y'
+        grid[4][4]='B'
+        '''
         # Place destination
         dy, dx = destination_pos
         if 0 <= dx < self.grid_size and 0 <= dy < self.grid_size:
             grid[dy][dx] = 'D'
-
+        '''
         # Place taxi
         ty, tx = taxi_pos
         if 0 <= tx < self.grid_size and 0 <= ty < self.grid_size:
@@ -150,8 +160,8 @@ class SimpleTaxiEnv(gym.Wrapper):
         # Print step info
         print(f"\nStep: {step}")
         print(f"Taxi Position: ({tx}, {ty})")
-        print(f"Passenger Position: ({px}, {py}) {'(In Taxi)' if (px, py) == (tx, ty) else ''}")
-        print(f"Destination: ({dx}, {dy})")
+        #print(f"Passenger Position: ({px}, {py}) {'(In Taxi)' if (px, py) == (tx, ty) else ''}")
+        #print(f"Destination: ({dx}, {dy})")
         print(f"Fuel Left: {fuel}")
         print(f"Last Action: {self.get_action_name(action)}\n")
 
@@ -176,24 +186,29 @@ def run_agent(agent_file, env_config, render=False):
     total_reward = 0
     done = False
     step_count = 0
+    stations = [(0, 0), (0, 4), (4, 0), (4,4)]
+    
+    taxi_row, taxi_col, _,_,_,_,_,_,_,_,obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look = obs
 
+    if render:
+        env.render_env((taxi_row, taxi_col),
+                       action=None, step=step_count, fuel=env.current_fuel)
+        time.sleep(0.5)
     while not done:
-        taxi_row, taxi_col, passenger_x, passenger_y, destination_x, destination_y, obstacle_north, obstacle_south, obstacle_east, obstacle_west = obs
-
-        if render:
-            env.render_env((taxi_row, taxi_col), (passenger_x, passenger_y), (destination_x, destination_y),
-                           action=None, step=step_count, fuel=env.current_fuel)
-            time.sleep(0.5)
+        
+        
         action = student_agent.get_action(obs)
 
-        obs, reward, done, _, _ = env.step(action)
+        obs, reward, done, _ = env.step(action)
+        print('obs=',obs)
         total_reward += reward
         step_count += 1
 
+        taxi_row, taxi_col, _,_,_,_,_,_,_,_,obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look,destination_look = obs
+
         if render:
-            env.render_env((obs[0], obs[1]), (obs[2], obs[3]), (obs[4], obs[5]),
+            env.render_env((taxi_row, taxi_col),
                            action=action, step=step_count, fuel=env.current_fuel)
-            time.sleep(0.5)
 
     print(f"Agent Finished in {step_count} steps, Score: {total_reward}")
     return total_reward
--- student_agent.py
+++ student_agent.py
@@ -6,13 +6,41 @@ import gym
 import torch
 
 def get_action(obs):
+    action = [0,1, 2, 3, 4, 5]
     
     # TODO: Train your own agent
     # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
-    
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
+    if obs[0]==0 or obs[10]==1:
+        action.remove(1)  
+    if obs[1]==0 or obs[12]==1:
+        action.remove(3)  
+    if obs[13]==1:
+        action.remove(2)
+    if obs[11]==1:
+        action.remove(0)
+    if obs[14]!=1:
+        action.remove(4)
+    if obs[15]!=1:
+        action.remove(5)
+
+    if action==[]:
+        print(obs)
+            
+    return random.choice(action) # Choose a random action
+
+    #return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 
+'''state = (taxi_row, taxi_col, self.stations[0][0],self.stations[0][1] ,self.stations[1][0],self.stations[1][1],self.stations[2][0],self.stations[2][1],self.stations[3][0],self.stations[3][1],obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look)
+
+ if action == 0 :  # Move South
+            next_row += 1
+        elif action == 1:  # Move North
+            next_row -= 1
+        elif action == 2 :  # Move East
+            next_col += 1
+        elif action == 3 :  # Move West
+            next_col -= 1'''
