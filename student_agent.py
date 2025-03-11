fd40bfd05e0a22621b4ca2ae1229ff29fd101c2e
gcobs104628 <gcobs104628@gmail.com>
2025-03-12 00:19:54 +0800

    test

--- student_agent.py
+++ student_agent.py
@@ -3,44 +3,38 @@ import numpy as np
 import pickle
 import random
 import gym
-import torch
+
+def get_state(obs):
+    taxi_row, taxi_col, _,_,_,_,_,_,_,_,obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look,destination_look = obs
+    return (taxi_row, taxi_col, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look)
+
+# Global variable to store the Q-table.
+q_table = None
+
+def load_q_table():
+    global q_table
+    if q_table is None:
+        with open("q_table.pkl", "rb") as f:
+            q_table = pickle.load(f)
+    return q_table
 
 def get_action(obs):
-    action = [0,1, 2, 3, 4, 5]
+    """
+    Given an observation, use the loaded Q-table to select an action.
+    If the observation key is missing, choose a random action as a fallback.
+    """
+    # Load Q-table if not already loaded.
+    qt = load_q_table()
     
-    # TODO: Train your own agent
-    # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
-    # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
-    #       To prevent crashes, implement a fallback strategy for missing keys. 
-    #       Otherwise, even if your agent performs well in training, it may fail during testing.
-    if obs[10]==1:
-        action.remove(1)  
-    if obs[13]==1:
-        action.remove(3)  
-    if obs[12]==1:
-        action.remove(2)
-    if obs[11]==1:
-        action.remove(0)
-    if obs[14]!=1:
-        action.remove(4)
-    if obs[15]!=1:
-        action.remove(5)
-
-    if len(action)==0:
-        print(obs)
-            
-    return random.choice(action) # Choose a random action
-
-    #return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
-    # You can submit this random agent to evaluate the performance of a purely random strategy.
-
-'''state = (taxi_row, taxi_col, self.stations[0][0],self.stations[0][1] ,self.stations[1][0],self.stations[1][1],self.stations[2][0],self.stations[2][1],self.stations[3][0],self.stations[3][1],obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look)
-
- if action == 0 :  # Move South
-            next_row += 1
-        elif action == 1:  # Move North
-            next_row -= 1
-        elif action == 2 :  # Move East
-            next_col += 1
-        elif action == 3 :  # Move West
-            next_col -= 1'''
+    # Convert the observation to a unique state key.
+    state_key = get_state(obs)
+    
+    # Check if the state key exists in the Q-table.
+    if state_key in qt:
+        # Exploit: select the action with the highest Q-value.
+        action = int(np.argmax(np.array(q_table[state_key])))
+    else:
+        # Fallback: choose a random action if the state is not present.
+        action = random.choice([0, 1, 2, 3, 4, 5])
+    
+    return action
